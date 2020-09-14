d2a2f23ee51396b35cc2dbcfce32f291424e5842
Sonam Wangchuk <somwangchotc90@gmail.com>
2020-09-14 08:46:55 +0100

    added static method

--- glakemap/sptref/spatialref.py
+++ glakemap/sptref/spatialref.py
@@ -4,24 +4,23 @@ import arcpy
 class SpatialReference():
 
 
-    def gcs(self, gcs_code):
+    @staticmethod
+    def gcs(gcs_code):
 
 
         """Returns geographic coordinate system"""
         
         
-        self.gcs_code = gcs_code
-        gcs = arcpy.SpatialReference(self.gcs_code) # 4326
+        gcs = arcpy.SpatialReference(gcs_code) # 4326
         return gcs
 
 
-
-    def pcs(self, pcs_code):
+    @staticmethod
+    def pcs(pcs_code):
         
 
         """Returns projected coordinate system"""
 
 
-        self.pcs_code = pcs_code
-        pcs = arcpy.SpatialReference(self.pcs_code)
+        pcs = arcpy.SpatialReference(pcs_code)
         return pcs
